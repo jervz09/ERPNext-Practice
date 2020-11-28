28ba6adc6864632092d1e3c0bb99a6c733498885
jervz09 <jervyariola@gmail.com>
2020-11-28 10:34:34 +0800

    Fix Showing member,items

--- apps/library_management/library_management/task.py
+++ apps/library_management/library_management/task.py
@@ -12,22 +12,21 @@ def daily():
 
     overdue = get_overdue(loan_period)
 
-    for member, items in overdue.iteritems():
+    for member, items in overdue.items():
         content = """<h2>Following Items are Overdue</h2>
         <p>Please return them as soon as possible</p><ol>"""
 
-        for i in items:
-            content += "<li>{0} ({1}) due on {2}</li>".format(i.article_name,
-                                                              i.article,
-                                                              format_date(add_days(i.transaction_date, loan_period)))
+        # for i in items:
+        #     content += "<li>{0} ({1}) due on</li>".format(i.name,
+        #                                                   i.article)
+        #
+        # content += "</ol>"
 
-        content += "</ol>"
-
-        frappe.log_error(content, "New Logging")
-        recipient = frappe.db.get_value("Library Member", member, "email_id")
-        frappe.sendmail(recipients=[recipient],
-                        sender="test@example.com",
-                        subject="Library Articles Overdue", content=content, bulk=True)
+        frappe.log_error(items, "New Logging {}".format(member))
+        # recipient = frappe.db.get_value("Library Member", member, "email_id")
+        # frappe.sendmail(recipients=[recipient],
+        #                 sender="test@example.com",
+        #                 subject="Library Articles Overdue", content=content, bulk=True)
 
 
 def get_overdue(loan_period):
@@ -37,8 +36,7 @@ def get_overdue(loan_period):
     overdue_by_member = {}
     articles_transacted = []
 
-    for d in frappe.db.sql("""select name, article, article_name,
-        library_member, member_name
+    for d in frappe.db.sql("""select *, date as transaction_date
         from `tabLibrary Transaction`
         order by transaction_date desc, modified desc""", as_dict=1):
 
@@ -46,8 +44,10 @@ def get_overdue(loan_period):
             continue
 
         if d.transaction_type == "Issue" and \
-                datediff(today, d.transaction_date) > loan_period:
+                date_diff(today, d.transaction_date) > loan_period:
             overdue_by_member.setdefault(d.library_member, [])
             overdue_by_member[d.library_member].append(d)
 
         articles_transacted.append(d.article)
+
+        return d
